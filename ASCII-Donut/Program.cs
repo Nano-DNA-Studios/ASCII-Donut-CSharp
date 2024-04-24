6b557d37d758a4ae6c77017e4bc632a72524abd2
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-04-24 18:16:17 -0400

    Project Init

---
+++ ASCII-Donut/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ASCII_Donut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
